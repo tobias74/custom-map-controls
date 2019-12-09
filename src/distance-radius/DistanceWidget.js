e9bfb4870b88ef751fdc5bf9aae39ade87181f55
root <root@AB9EFA7>
2019-12-09 15:24:37 +0000

    add Radius-Distance-Tool

---
+++ src/distance-radius/DistanceWidget.js
@@ -0,0 +1,62 @@
+import { RadiusWidget } from './RadiusWidget.js';
+
+export class DistanceWidget extends google.maps.MVCObject {
+
+  constructor(map, options) {
+    super();
+    this.options = options;
+    console.log('making the distance widget', this.options);
+    this.set('map', map);
+    this.set('position', map.getCenter());
+
+    var marker = new google.maps.Marker({
+      draggable: true,
+      title: 'Move me!'
+    });
+
+    // Bind the marker map property to the DistanceWidget map property 
+    marker.bindTo('map', this);
+
+    // Bind the marker position property to the DistanceWidget position 
+    // property 
+    marker.bindTo('position', this);
+
+
+
+    // Create a new radius widget 
+    this.radiusWidget = new RadiusWidget(this.options);
+
+  }
+
+  enableRadius() {
+    this.radiusWidget.enableMe();
+
+    // Bind the radiusWidget map to the DistanceWidget map 
+    this.radiusWidget.bindTo('map', this);
+
+    // Bind the radiusWidget center to the DistanceWidget position 
+    this.radiusWidget.bindTo('center', this, 'position');
+
+    // Bind to the radiusWidgets' distance property 
+    this.bindTo('distance', this.radiusWidget);
+
+    // Bind to the radiusWidgets' bounds property 
+    this.bindTo('bounds', this.radiusWidget);
+
+  }
+
+  disableRadius() {
+    this.radiusWidget && this.radiusWidget.disableMe();
+  }
+
+  distance_changed() {
+    this.options.onNewSearchRadius(this.get('position'), Math.round(this.get('distance')));
+  }
+
+  position_changed() {
+    console.log(this);
+    this.options.onNewSearchRadius(this.get('position'), Math.round(this.get('distance')));
+  }
+
+
+}
---
+++ src/distance-radius/RadiusWidget.js
@@ -0,0 +1,70 @@
+export class RadiusWidget extends google.maps.MVCObject {
+
+  constructor(defaults) {
+    super();
+    this.circle = new google.maps.Circle({
+      strokeWeight: 2,
+      draggable: true
+    });
+
+    this.set('distance', defaults.maxDistance);
+
+    this.sizer = new google.maps.Marker({
+      draggable: true,
+      icon: {
+        url: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png",
+        size: new google.maps.Size(15, 15),
+        anchor: new google.maps.Point(4, 4)
+      },
+      title: 'Drag me!'
+    });
+  }
+
+  enableMe() {
+    this.bindTo('bounds', this.circle);
+    this.circle.bindTo('center', this);
+    this.circle.bindTo('map', this);
+    this.circle.bindTo('radius', this);
+    this.enableSizer();
+  }
+
+  disableMe() {
+    this.unbindAll();
+    this.disableSizer();
+    this.circle.unbindAll();
+    this.circle.setMap(null);
+  }
+
+  distance_changed() {
+    this.set('radius', this.get('distance'));
+  }
+
+  enableSizer() {
+    this.sizer.bindTo('map', this);
+
+    google.maps.event.addListener(this.sizer, 'drag', () => {
+      var pos = this.sizer.get('position');
+      var center = this.get('center');
+      var distance = google.maps.geometry.spherical.computeDistanceBetween(center, pos);
+      this.set('distance', distance);
+    });
+  }
+
+  disableSizer() {
+    this.sizer.unbindAll();
+    google.maps.event.clearListeners(this.sizer);
+    this.sizer.setMap(null);
+  }
+
+
+  center_changed() {
+    var bounds = this.get('bounds');
+
+    if (bounds) {
+      var lng = bounds.getNorthEast().lng();
+      this.sizer.set('position', new google.maps.LatLng(this.get('center').lat(), lng));
+    }
+  }
+
+
+}
